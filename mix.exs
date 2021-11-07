2116230aa6984dab04cdee8a9ed09ab526dc75a6
Rayan Castro <rayanrcastro@gmail.com>
2021-11-06 23:19:17 -0300

    chore: add ecto pheonix_ecto and mongodb dependeces

--- mix.exs
+++ mix.exs
@@ -45,7 +45,10 @@ defmodule Workshop.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ecto, "~> 3.7.1"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:mongodb, git: "https://github.com/elixir-mongo/mongodb.git", branch: "otp/24"}
     ]
   end
 
